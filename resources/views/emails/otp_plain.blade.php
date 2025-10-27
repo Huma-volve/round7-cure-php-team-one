0787e828a7b132fc831e4fd3537e209456ed78b4
reem <mreem6985@gmail.com>
2025-10-28 01:05:43 +0300

    register/sending otp via email /otp verification/logout

---
+++ resources/views/emails/otp_plain.blade.php
@@ -0,0 +1,9 @@
+
+ Email Verification
+    
+       
+        Hello! Please use the following verification code to verify your email address:{{ $otp }}
+
+        This code will expire in 5 minutes
+        If you didn’t request this, please ignore this email.
+    
