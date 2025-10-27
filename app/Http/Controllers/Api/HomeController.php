64f1b4fe1069056cbf4b67fd29e28527f61e8441
Eslam Mohamed <eslamemam39@gmail.com>
2025-10-27 22:23:34 +0300

    Add doctors module: migrations, models, services, seeders and search API feature

---
+++ app/Http/Controllers/Api/HomeController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Doctor;
+use App\Services\FavoriteService;
+use App\Models\User;
+use App\Services\DoctorService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+
+   protected $doctorService;
+   protected $favoriteService;
+
+    public function __construct(DoctorService $doctorService , FavoriteService $favoriteService)
+    {
+        $this->favoriteService = $favoriteService;
+        $this->doctorService = $doctorService;
+    }
+
+    public function index(Request $request)
+    {
+
+    $request->validate([
+        'latitude' => 'required|numeric',
+        'longitude' => 'required|numeric',
+    ]);
+
+    $user = Auth::user() ?? User::find(1);
+    $lat = $request->latitude ?? 30.0444; // Cairo center
+    $lng = $request->longitude ?? 31.2357;
+    $search = $request->input('search');
+
+
+    $doctors = $this->doctorService->getNearbyDoctors($user, $lat, $lng, $search);
+
+       return response()->json(['data' => $doctors]);
+
+    }
+
+
+    public function toggleFavorite($doctorId)
+{
+    $doctor = Doctor::findOrFail($doctorId);
+    $user = Auth::user() ?? User::find(1);
+
+    $status = $this->favoriteService->toggleFavorite($user, $doctor);
+
+    return response()->json([
+        'message' => $status == 'added' ? 'Added to favorites' : 'Removed from favorites',
+        'status' => $status
+    ]);
+}
+}
---
+++ app/Services/DoctorService.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Services;
+use App\Models\User;
+use App\Models\Doctor;
+use App\Services\FavoriteService;
+
+
+class DoctorService  {
+
+    protected $favoriteService;
+
+    public function __construct(FavoriteService $favoriteService)
+    {
+        $this->favoriteService = $favoriteService;
+    }
+
+    public function getNearbyDoctors(User $user, $latitude, $longitude, $search = null , $radius =10){
+
+       $doctors = Doctor::with('user')->selectRaw(
+            "*, (6371 * acos(cos(radians(?)) * cos(radians(latitude)) * cos(radians(longitude) - radians(?)) + sin(radians(?)) * sin(radians(latitude)))) AS distance",
+             [$latitude, $longitude, $latitude]
+        )
+        ->when($search, function($query, $search) {
+            $query->whereHas('user', function($q) use ($search) {
+                $q->where('name', 'like', "%$search%");
+            })
+             ->orWhereHas('specialty', function($q) use ($search) {
+               $q->where('name', 'like', "%$search%");
+             });
+
+        })
+        ->havingRaw("distance < ?", [$radius])
+        ->orderBy("distance", "asc")
+        ->get();
+
+        $doctors->each(function($doctor) use ($user) {
+            $doctor->is_favorite = $this->favoriteService->isFavorite($user, $doctor);
+        });
+
+        return $doctors;
+
+
+    } //end getNearbyDoctors
+
+
+    public function getDoctorDetails($doctorId, $user = null){
+        
+        $doctor = Doctor::with('user', 'specialty' )->findOrFail($doctorId);
+
+        // if($user){
+        //     $doctor->is_favorite = $this->favoriteService->isFavorite($user, $doctor);
+        // } else {
+        //     $doctor->is_favorite = false;
+        // }
+
+
+        return $doctor;
+    } //end getDoctorDetails
+
+
+
+
+}
