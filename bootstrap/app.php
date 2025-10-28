fe5962f5af7a0bee2626c533eb06d478ccbbb69a
israa <israairfan99@gmail.com>
2025-10-28 18:34:51 +0300

    Initial commit for esraa-ragab branch

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,9 @@
 <?php
-
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +16,16 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Record not found.',
+                    'data' => null
+                ], 404);
+            }
+
+            return null; 
+        });
+    })
+    ->create();
