d75a33a3edee035112ac8bbb0941b3a9a6f6e683
reem <mreem6985@gmail.com>
2025-11-03 00:57:04 +0200

    doctor profile management

--- app/Traits/HandlesRoleUpdates.php
+++ app/Traits/HandlesRoleUpdates.php
@@ -1,38 +1,4 @@
 <?php
-
-namespace App\Traits;
-
-use Illuminate\Http\Request;
-
-// trait HandlesRoleUpdates
-// {
-//     protected function handlePatientUpdate($user, Request $request)
-//     {
-//         $user->patient()->update([
-//             'birthdate' => $request->birthdate ?? $user->birthdate,
-//             'gender' => $request->gender ?? $user->gender,
-//             'medical_notes' => $request->medical_notes ?? optional($user->patient)->medical_notes,
-//         ]);
-//     }
-
-//     protected function handleDoctorUpdate($user, Request $request)
-//     {
-//         $user->doctor()->update([
-//             'specialty' => $request->specialty ?? optional($user->doctor)->specialty,
-//             'license_number' => $request->license_number ?? optional($user->doctor)->license_number,
-//             'clinic_location' => $request->clinic_location ?? optional($user->doctor)->clinic_location,
-//             'session_price' => $request->session_price ?? optional($user->doctor)->session_price,
-//             'availability_json' =>$request->availability_json ?? optional($user->doctor)->availability_json,
-//             'experience' =>$request->experience ?? optional($user->doctor)->experience,
-//             'about_me' => $request->about_me ?? optional($user->doctor)->about_me,
-//             'latitude' => $request->latitude ?? optional($user->doctor)->latitude,
-//             'longitude' => $request->longitude ?? optional($user->doctor)->longitude,
-//         ]);
-//     }
-// }
-
-
-
 namespace App\Traits;
 
 use Illuminate\Http\Request;
@@ -58,25 +24,68 @@ trait HandlesRoleUpdates
     {
         $data = [];
 
-        foreach ([
-            'specialty',
-            'license_number',
-            'clinic_location',
-            'session_price',
-            'availability_json',
-            'experience',
-            'about_me',
-            'latitude',
-            'longitude'
-        ] as $field) {
+        foreach (
+            [
+                'specialty',
+                'license_number',
+                'clinic_location',
+                'session_price',
+                'experience',
+                'about_me',
+                'latitude',
+                'longitude'
+            ] as $field
+        ) {
             if ($request->filled($field)) {
                 $data[$field] = $request->$field;
             }
         }
+
+
+        if ($request->has('availability_json')) {
+            $oldAvailability = json_decode($user->availability_json ?? '{}', true);
+            $dataToUpdate = json_decode($request->availability_json, true);
+
+            if (isset($dataToUpdate['add'])) {
+                foreach ($dataToUpdate['add'] as $day => $times) {
+                    if (!isset($oldAvailability[$day])) {
+                        $oldAvailability[$day] = [];
+                    }
+
+                    $oldAvailability[$day] = array_unique(array_merge(
+                        (array) $oldAvailability[$day],
+                        (array) $times
+                    ));
+                }
+            }
+
+            if (isset($dataToUpdate['remove'])) {
+                foreach ($dataToUpdate['remove'] as $day => $times) {
+                    if (isset($oldAvailability[$day])) {
+                        $oldAvailability[$day] = array_values(array_diff(
+                            (array) $oldAvailability[$day],
+                            (array) $times
+                        ));
+
+                        if (empty($oldAvailability[$day])) {
+                            unset($oldAvailability[$day]);
+                        }
+                    }
+                }
+            }
+
+
+            $user->availability_json = json_encode($oldAvailability);
+            $dataToUpdate = [
+                'availability_json' => $user->availability_json,
+            ];
+            $user->doctor->update($dataToUpdate);
+        }
+
+
 
         if (!empty($data)) {
             $user->doctor()->update($data);
         }
     }
 }
-
