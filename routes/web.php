bdf1e8f9aae4e47202917830ba5761a7ff75ae33
Eslam Mohamed <eslamemam39@gmail.com>
2025-11-03 18:39:12 +0200

    resolved confelct

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('admin.index');
+    return view('welcome');
 });
+
+
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+})->name('dashboard');
+
+
+
+require __DIR__.'/web/doctor.php';
