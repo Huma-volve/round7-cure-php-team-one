a5ab83bef846f7b5888cac4c139e5dd00c1a0332
reem <mreem6985@gmail.com>
2025-11-05 10:58:21 +0200

    solve conflict

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,26 +14,16 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-
-
-        // 'id' => $this->id,
-        // 'name' => $this->name,
-        // 'email' => $this->email,
-        // 'mobile' => $this->mobile,
-        // 'birthdate' => $this->birthdate,
-        // 'profile_photo' => $this->profile_photo ? asset($this->profile_photo) : null,
-        // 'role' => $this->getRoleNames()->first(),
-        // 'patient' => [
-        //     'birthdate' => optional($this->patient)->birthdate,
-        //     'gender' => optional($this->patient)->gender,
-        //     'medical_notes' => optional($this->patient)->medical_notes,
-        // ],
         return [
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
             'mobile' => $this->mobile,
+<<<<<<< Updated upstream
             'birthdate' => optional($this->birthdate)->format('Y-m-d'),
+=======
+            'birthdate' => $this->birthdate ? $this->birthdate->format('Y-m-d') : null,
+>>>>>>> Stashed changes
             'profile_photo' => $this->profile_photo ? asset($this->profile_photo) : null,
             'role' => $this->getRoleNames()->first(),
 
@@ -42,7 +32,11 @@ class UserResource extends JsonResource
 
 
             'patient' => $this->when($this->hasRole('patient'), [
+<<<<<<< Updated upstream
                'birthdate' => optional(optional($this->patient)->birthdate)->format('Y-m-d'),
+=======
+                'birthdate' => optional($this->patient)->birthdate->format('Y-m-d'),
+>>>>>>> Stashed changes
                 'gender' => optional($this->patient)->gender,
                 'medical_notes' => optional($this->patient)->medical_notes,
             ]),
