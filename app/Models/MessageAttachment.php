3309c52ea4cac9854cfd96cb3ec47256a507c557
Ahmed abdeldaym <ahmedabdeldaem01000@gmail.com>
2025-11-05 15:32:01 +0200

    update Api

--- app/Models/MessageAttachment.php
+++ app/Models/MessageAttachment.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MessageAttachment extends Model
 {
-    use HasFactory;
+    use HasFactory ,SoftDeletes;
 
     protected $fillable = [
         'message_id',
@@ -15,6 +16,7 @@ class MessageAttachment extends Model
         'url',
         'size',
     ];
+    protected $dates = ['deleted_at'];
 
     public function message()
     {
